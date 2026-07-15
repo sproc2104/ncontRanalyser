e14b9052cdae20a716e63098b4840c098e09b15b
sproc <saraprocacci04@gmail.com>
2026-07-15 13:49:53 +0100

    fixing mistakes in the save function

--- R/save_function.R
+++ R/save_function.R
@@ -1,6 +1,6 @@
 ############ SAVE FUNCTION
 
-save_txt<- function(output, interaction_name){
+save_csv<- function(output, interaction_name){
 
   #first label every table with a H or k column to tell them apart
   h_ab_table<- cbind(chain="H", ouput[[3]])
@@ -12,8 +12,8 @@ save_txt<- function(output, interaction_name){
   ab_output<- rbind(h_ab_table, k_ab_table)
   ag_output<- rbind(h_ag_table, k_ag_table)
 
-  write.csv(ab_output, paste0(output_name, "_antibody_afpp"))
+  write.csv(ab_output, paste0(output_name, "_antibody_afpp.csv"))
   write.csv(ag_output, paste0(output_name, "_antigen_afpe.csv"))
 
-  print(paste0("Files saved as:\n ", paste0(output_name, "_antibody_afpp/n", output_name, "_antigen_afpe.csv")))
+  print(paste0("Files saved as:\n ", paste0(output_name, "_antibody_afpp.csv/n", output_name, "_antigen_afpe.csv")))
 }
