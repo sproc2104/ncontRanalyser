114661db5061efc991226d8977ff4ef3f0dc00e5
sproc <saraprocacci04@gmail.com>
2026-07-22 06:35:02 +0100

    changing save_csv function

--- R/save_function.R
+++ R/save_function.R
@@ -1,9 +1,9 @@
 ############ SAVE FUNCTION
 
-save_csv<- function(output, interaction_name){
+save_csv<- function(output, antibody_output, antigen_output){
 
   #first label every table with a H or k column to tell them apart
-  h_ab_table<- cbind(chain="H", ouput[[3]])
+  h_ab_table<- cbind(chain="H", output[[3]])
   k_ab_table<- cbind(chain="k", output[[4]])
   h_ag_table<- cbind(int_chain="H", output[[5]])
   k_ag_table<- cbind(int_chain="k", output[[6]])
@@ -12,8 +12,7 @@ save_csv<- function(output, interaction_name){
   ab_output<- rbind(h_ab_table, k_ab_table)
   ag_output<- rbind(h_ag_table, k_ag_table)
 
-  write.csv(ab_output, paste0(output_name, "_antibody_afpp.csv"))
-  write.csv(ag_output, paste0(output_name, "_antigen_afpe.csv"))
+  write.csv(ab_output, antibody_output)
+  write.csv(ag_output, antigen_output)
 
-  print(paste0("Files saved as:\n ", paste0(output_name, "_antibody_afpp.csv/n", output_name, "_antigen_afpe.csv")))
 }
